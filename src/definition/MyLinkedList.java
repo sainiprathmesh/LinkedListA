bc5657c34e1b12ad82b1e31d605c25dab621251a
sainiprathmesh <sainiprathmesh@gmail.com>
2020-01-23 10:59:56 +0530

    create the inner class : Node

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -3,6 +3,11 @@ package definition;
 import adt.LinkedListADT;
 
 public class MyLinkedList<E> implements LinkedListADT<E> {
+
+    private static class Node {
+
+    }
+
     @Override
     public void add(int index, E item) {
 
